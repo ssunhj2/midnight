8386b0008bda14a7b61a601635b80345aa300e71
es2sun <wiiro017@gmail.com>
2021-02-27 12:14:32 +0900

    다이나믹 프로그래밍 문제 - 타일링 문제풀이 commit

--- src/com/sun/dynamicProgramming/Tiling.java
+++ src/com/sun/dynamicProgramming/Tiling.java
@@ -1,5 +1,8 @@
 package com.sun.dynamicProgramming;
 
+import com.sun.common.CaseNumber.RandomNumber;
+
+// 2xN 타일링 문
 public class Tiling
 {
     /**
@@ -19,7 +22,26 @@ public class Tiling
 
     public static void main(String[] args)
     {
+        int caseNumber = RandomNumber.intRandomNumber(1000, 1);
 
+        int result = caseCnt(caseNumber) / 10007;
+        System.out.println("2x" + caseNumber + " 크기의 직사각형을 채우는 방법의 수 를 10,007로 나눈 수:  " + result);
 
     }
+
+    // 한번 구한 값은 배열에 저장한다.
+    private static int[] caseCntArr = new int[1000];
+    // 1. 피보나치와 같이 경우의 수가 1, 2, 3, 5, 8 ...로 늘어난다.(n=1, 2, .. 일때)
+    // 2. 1x2 타일로 채우는 방법의 수 n-1
+    //    2x1 타일로 채우는 방법의 수 n-2
+    private static int caseCnt(int n)
+    {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(caseCntArr[n] != 0) return caseCntArr[n];
+
+        caseCntArr[n] = caseCnt(n-1) + caseCnt(n-2);
+        return caseCntArr[n];
+    }
+
 }
